9b4d99363bf6acff00acbc7014f3b40eea756da2
Anders Thomsen <ande605x@edu.easj.dk>
2017-03-13 13:55:36 +0100

    Add project files.

---
+++ Hotel Enitity Framework/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Enitity_Framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
