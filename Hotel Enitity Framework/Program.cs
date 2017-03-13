c3da4da257149ee1d282f29865a88936cb947d6d
Anders Thomsen <ande605x@edu.easj.dk>
2017-03-13 14:13:17 +0100

    Opg1

--- Hotel Enitity Framework/Program.cs
+++ Hotel Enitity Framework/Program.cs
@@ -10,6 +10,29 @@ namespace Hotel_Enitity_Framework
     {
         static void Main(string[] args)
         {
+
+            using (var db = new HotelContext())
+            {
+                var allehoteller = from h in db.Hotel
+                                   select h;
+                
+                foreach(var item in allehoteller)
+                {
+                    Console.WriteLine(item.Hotel_No+" "+item.Name+" "+item.Address);
+                }
+
+                Console.WriteLine();
+
+                var allekunder = from g in db.Guest
+                                 select g;
+
+                foreach(var item in allekunder)
+                {
+                    Console.WriteLine(item.Guest_No+"   "+item.Name+" "+item.Address+"  "+item.Booking);
+                }
+
+                Console.ReadLine();
+            }
         }
     }
 }
